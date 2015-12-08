d328a82c11ed53a691101bd96cb9281f0be7bf5a
scavenius <scaveothesanguine@gmail.com>
2015-12-08 15:56:32 +0100

    instanciator basic methods added has yet to test

--- TicketReservationSystem/controller/Instanciator.java
+++ TicketReservationSystem/controller/Instanciator.java
@@ -8,6 +8,7 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by scavenius on 12/8/15.
@@ -26,11 +27,11 @@ public final class Instanciator {
     static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
     static final String DB_URL = "jdbc:mysql://mysql.itu.dk/" + DB;
 
-    public static ArrayList<Theater> createTheaterList() {
+    public static HashMap<String, Theater> createTheaterMap() {
 
         Connection connection = null;
         Statement statement = null;
-        ArrayList<Theater> returnArray = new ArrayList<Theater>();
+        HashMap<String, Theater> returnMap = new HashMap<String, Theater>();
 
         try {
             DriverManager.registerDriver(new com.mysql.jdbc.Driver()); // STEP 2: Register JDBC driver
@@ -40,17 +41,17 @@ public final class Instanciator {
             String sql = "SELECT * FROM theaters ORDER BY theaters , theater_identifier ASC LIMIT 0 , 30";
             ResultSet rs = statement.executeQuery(sql);
             //STEP 5: Extract data from result set
-            while (rs.next()) { //Retrieve by column name
-                int time = rs.getInt("time");
-                String date = rs.getString("date");
-                String film = rs.getString("Film");
 
-                // int id  = rs.getInt("id");
-                String theater = rs.getString("theater_identifier");
-                //String name = rs.getString("seats");
-                //Display values
-                System.out.println("theater: '" + theater + "'");
+            while (rs.next()) { //Retrieve by column name
+
+                int seats = rs.getInt("seats");
+                int rows = rs.getInt("rows");
+                String identifier = rs.getString("theater_identifier");
+
+                returnMap.put(identifier, new Theater(identifier, seats, rows));
+
             }
+
             rs.close();
             connection.close();
         } catch(Exception e) {
@@ -58,7 +59,7 @@ public final class Instanciator {
             e.printStackTrace();
         }
 
-        return returnArray;
+        return returnMap;
     }
 
     public static ArrayList<Show> createShowlist() {
@@ -67,6 +68,8 @@ public final class Instanciator {
         Statement statement = null;
         ArrayList<Show> returnArray = new ArrayList<Show>();
 
+        HashMap<String, Theater> theaterMap = createTheaterMap();
+
         try {
             DriverManager.registerDriver(new com.mysql.jdbc.Driver()); // STEP 2: Register JDBC driver
             connection = DriverManager.getConnection(DB_URL, USER, PASS); // STEP 3: Open a connection
@@ -74,17 +77,17 @@ public final class Instanciator {
 
             String sql = "SELECT * FROM shows ORDER BY shows , date ASC LIMIT 0 , 30";
             ResultSet rs = statement.executeQuery(sql);
+
             //STEP 5: Extract data from result set
             while (rs.next()) { //Retrieve by column name
+
                 int time = rs.getInt("time");
                 String date = rs.getString("date");
                 String film = rs.getString("Film");
-
-                // int id  = rs.getInt("id");
                 String theater = rs.getString("theater_identifier");
-                //String name = rs.getString("seats");
-                //Display values
-                System.out.println("theater: '" + theater + "'");
+
+                returnArray.add(new Show(theaterMap.get(theater), film, date, time));
+
             }
             rs.close();
             connection.close();
